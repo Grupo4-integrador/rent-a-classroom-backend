7754964d49d139f4148ed2f4d6c7d21b5226ff13
gustavomullerf <gustavoeduardomf@gmail.com>
2023-03-27 20:54:41 -0300

    Adicionando application.properties ao projeto e corrigindo RentaclassroomApplication.java

--- src/main/java/br/com/uniamerica/rentaclassroom/repository/SalaRepository.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/repository/SalaRepository.java
@@ -1,7 +1,6 @@
 package br.com.uniamerica.rentaclassroom.repository;
 
 import br.com.uniamerica.rentaclassroom.entitys.Sala;
-import br.com.uniamerica.rentaclassroom.entitys.SelecaoMaterial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SalaRepository extends JpaRepository<Sala, Long> {
