8130fade0d40707e71699111e3401e9b56ad19d5
Kauê Sanches <kauesanches123@gmail.com>
2023-03-27 20:45:12 -0300

    testando ide

--- src/main/java/br/com/uniamerica/rentaclassroom/RentaclassroomApplication.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/RentaclassroomApplication.java
@@ -3,6 +3,7 @@ package br.com.uniamerica.rentaclassroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// testando
 @SpringBootApplication
 public class RentaclassroomApplication {
 
