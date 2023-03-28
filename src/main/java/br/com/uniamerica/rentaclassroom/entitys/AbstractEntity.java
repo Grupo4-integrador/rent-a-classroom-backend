f88f8e57cab5a7faf29f3b6b6c48fb8773c1af5c
Kauê Sanches <kauesanches123@gmail.com>
2023-03-27 21:28:57 -0300

    Inicio cardinalidades nas classes AbstractEntity & Agendamento

--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/AbstractEntity.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/AbstractEntity.java
@@ -1,10 +1,34 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tb_ids", schema = "rentaclassroom")
 public abstract class AbstractEntity {
+    @Id
+    @Getter @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, unique = true)
     private Long id;
+    @Getter @Setter
+    @Column(name = "cadastro", nullable = false)
     private LocalDateTime cadastro;
-    private LocalDateTime edicao;
+    @Setter @Getter
+    @Column(name = "atualizacao", nullable = false)
+    private LocalDateTime atualizacao;
+    @Setter @Getter
+    @Column(name = "ativo", nullable = false)
     private boolean ativo;
+
+    @PrePersist
+    public void dataCadastro(){
+        this.setCadastro(LocalDateTime.now());
+    }
+    @PreUpdate
+    public void dataAtualizacao(){
+        this.setAtualizacao(LocalDateTime.now());
+    }
 }
--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/Agendamento.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/Agendamento.java
@@ -1,12 +1,30 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tb_agendamentos", schema = "rentaclassroom")
 public class Agendamento extends AbstractEntity{
+    @Setter @Getter
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "tb_professor", nullable = false)
     private Professor profesor;
+    @Getter @Setter
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "tb_ambiente", nullable = false)
     private Sala ambiente;
+    @Getter @Setter
+    @Column(name = "data", nullable = false)
     private LocalDateTime data;
+    @Getter @Setter
+    @JoinColumn(name = "tb_periodo", nullable = false)
     private Periodo periodo;
+    @Getter @Setter
+    @Column(name = "horaInicio", nullable = false)
     private LocalDateTime horaInicio;
     private LocalDateTime horaFim;
     private boolean solicitaMaterial;
