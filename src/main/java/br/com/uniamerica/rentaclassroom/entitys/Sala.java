f370f6491a99170fd0a162642eb245be1276222d
gabrielruizmb <105463724+gabrielruizmb@users.noreply.github.com>
2023-04-03 19:17:31 -0300

    adcionado anotações

--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/Sala.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/Sala.java
@@ -1,8 +1,22 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
-public class Sala extends AbstractEntity{
+@Entity
+@Table(name = "salas", schema = "public")
+public class Sala extends AbstractEntity {
+    
+    @Getter @Setter
+    @Column(name = "nome", nullable = false, unique = true)
     private String nome;
+    
+    @Getter @Setter
+    @Column(name = "capacidade", nullable = false)
     private int capacidade;
+    
+    @Getter @Setter
+    @Column(name = "andar", nullable = false)
     private Andar andar;
+    
+    @Getter @Setter
+    @Column(name = "descricao", nullable = false)
     private String descricao;
 }
