4218a262a92b505ea4bb532d34929867b0ab2af4
gustavomullerf <gustavoeduardomf@gmail.com>
2023-04-03 19:39:53 -0300

    annotation classe professor e selecao_material

--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/Professor.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/Professor.java
@@ -1,8 +1,23 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_professor", schema = "rentaclassroom")
 public class Professor extends AbstractEntity{
+    @Getter
+    @Column(name = "ra", nullable = false, unique = true)
     private Long ra;
+    @Getter @Setter
+    @Column(name = "nome", nullable = false, length = 50)
     private String nome;
+    @Getter @Setter
+    @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
+    @Getter @Setter
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "turma", nullable = false)
     private Turma turma;
 }
--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/SelecaoMaterial.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/SelecaoMaterial.java
@@ -1,6 +1,18 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_selecao_material", schema = "rentaclassroom")
 public class SelecaoMaterial extends AbstractEntity {
+
+    @Getter @Setter
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "tb_materiais", nullable = false)
     private Material materiais;
+    @Getter @Setter
+    @Column(name = "quantidade", nullable = false)
     private int quantidade;
 }
