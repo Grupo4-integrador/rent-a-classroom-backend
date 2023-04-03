34d80b4455a2b46f7e27947044de0e2b15bc66bc
Kauê Sanches <kauesanches123@gmail.com>
2023-04-03 19:47:19 -0300

    anotacoes prontas

--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/Turma.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/Turma.java
@@ -1,7 +1,19 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_turma", schema = "rentaclassroom")
 public class Turma extends AbstractEntity{
-    private Professor responsavel;
-    private int quantAlunos;
+    @Getter @Setter
+    @Column(name = "curso", nullable = false, unique = true, length = 50)
     private String curso;
+    @Getter @Setter
+    @Column(name = "quant_alunos", nullable = false)
+    private int quantAlunos;
 }
