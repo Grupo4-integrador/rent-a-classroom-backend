8c4aff6facf1d90949349d50dcea9ab4ac701f44
marcelonv011 <marcelonv011@gmail.com>
2023-04-19 19:49:55 -0300

    chamada repository

--- src/main/java/br/com/uniamerica/rentaclassroom/repository/AgendamentoRepository.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/repository/AgendamentoRepository.java
@@ -2,6 +2,8 @@ package br.com.uniamerica.rentaclassroom.repository;
 
 import br.com.uniamerica.rentaclassroom.entitys.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 }
