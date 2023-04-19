75846e5f1a031bbf1bedde2f78df0ffb41763462
adrieli johann <103552296+adrielijohann@users.noreply.github.com>
2023-04-19 19:51:32 -0300

    chamada repository

--- src/main/java/br/com/uniamerica/rentaclassroom/repository/AgendamentoRepository.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/repository/AgendamentoRepository.java
@@ -3,5 +3,6 @@ package br.com.uniamerica.rentaclassroom.repository;
 import br.com.uniamerica.rentaclassroom.entitys.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@repository
 public interface AgendamentoRepository
   extends JpaRepository<Agendamento, Long> {}
