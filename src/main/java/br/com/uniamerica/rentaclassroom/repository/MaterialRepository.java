f51cb247d5f3f4e9aeb0adc092558042e4357f54
marcelonv011 <marcelonv011@gmail.com>
2023-04-19 20:40:09 -0300

    adicionando repositorys

--- src/main/java/br/com/uniamerica/rentaclassroom/repository/MaterialRepository.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/repository/MaterialRepository.java
@@ -3,6 +3,6 @@ package br.com.uniamerica.rentaclassroom.repository;
 import br.com.uniamerica.rentaclassroom.entitys.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+@Repository
 public interface MaterialRepository extends JpaRepository<Material, Long> {}
 
--- src/main/java/br/com/uniamerica/rentaclassroom/repository/TurmaRepository.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/repository/TurmaRepository.java
@@ -4,4 +4,5 @@ import br.com.uniamerica.rentaclassroom.entitys.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long> {}
