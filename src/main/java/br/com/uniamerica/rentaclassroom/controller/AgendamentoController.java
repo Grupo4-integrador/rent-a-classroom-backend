fee5b1e42fda868595e2cd31ea8ece4ad3474cc8
Kauê Sanches <kauesanches123@gmail.com>
2023-04-28 19:33:26 -0300

    Criando controllers

---
+++ src/main/java/br/com/uniamerica/rentaclassroom/controller/AgendamentoController.java
@@ -0,0 +1,17 @@
+package br.com.uniamerica.rentaclassroom.controller;
+
+import br.com.uniamerica.rentaclassroom.repository.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/agendamento")
+public class AgendamentoController {
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    @GetMapping(value = "")
+}
---
+++ src/main/java/br/com/uniamerica/rentaclassroom/controller/MaterialController.java
@@ -0,0 +1,4 @@
+package br.com.uniamerica.rentaclassroom.controller;
+
+public class MaterialController {
+}
