2edb89d12e0c0e9f5a94439b55ebb8963d2b07dd
marcelonv011 <marcelonv011@gmail.com>
2023-04-30 18:20:22 -0300

    adicionando sala controller com os metodos

--- src/main/java/br/com/uniamerica/rentaclassroom/controller/SalaController.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/controller/SalaController.java
@@ -1,20 +1,90 @@
 package br.com.uniamerica.rentaclassroom.controller;
 
+import br.com.uniamerica.rentaclassroom.entitys.Sala;
 import br.com.uniamerica.rentaclassroom.repository.SalaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(value = "/api/sala")
+@RequestMapping(value = "api/sala")
 public class SalaController {
+
     @Autowired
     private SalaRepository salaRepository;
 
-    //@GetMapping("/{id}")
-    //@GetMapping
-    //@GetMapping
-    //@PutMapping
-    //@PostMapping
-    //@DeleteMapping
+    @GetMapping("/{id}")
+    public ResponseEntity <?> findByIdPath(@PathVariable("id") final Long id){
+        final Sala sala = this.salaRepository.findById(id).orElse(null);
+        return sala == null ? ResponseEntity.badRequest().body("Nenhum valor foi encontrado") : ResponseEntity.ok(sala);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findByRequest(
+            @RequestParam("id") final long id
+    ) {
+        final Sala sala = this.salaRepository.findById(id).orElse(null);
+        return sala == null
+                ? ResponseEntity.badRequest().body("Nenhum valor foi encontrado")
+                : ResponseEntity.ok(sala);
+    }
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(this.salaRepository.findAll());
+    }
+
+
+     @GetMapping("ativo")
+    public ResponseEntity<?> findByAtivo(){
+        return ResponseEntity.ok(this.salaRepository.findByAtivo(true));
+     }
+
+     @PostMapping
+    public ResponseEntity<?> cadastar(
+            @RequestBody final Sala sala
+     ) {
+        try {
+            this.salaRepository.save(sala);
+            return ResponseEntity.ok().body("Registro realizado com sucesso");
+        }
+        catch (Exception e) {
+            return ResponseEntity.badRequest().body("error" + e.getMessage());
+        }
+     }
+
+     @PutMapping
+    public ResponseEntity<?> atualizar(
+            @RequestParam("id") final long id,
+            @RequestBody final Sala sala
+     ) {
+        try {
+            final Sala salaBanco = this.salaRepository.findById(id).orElse(null);
+
+            if(salaBanco == null || !salaBanco.getId().equals(sala.getId())){
+                throw new RuntimeException("nao foi possivel identificar o registro informado.");
+            }
+
+            this.salaRepository.save(sala);
+            return ResponseEntity.ok().body("Registro atualizado com sucesso");
+        }
+        catch (DataIntegrityViolationException e){
+            return ResponseEntity.internalServerError().body("error" + e.getCause().getCause().getMessage());
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body("error" + e.getMessage());
+        }
+     }
+
+     @DeleteMapping
+    public ResponseEntity<?> excluir(
+            @RequestParam("id") final long id
+     ){
+        final Sala sala = this.salaRepository.findById(id).orElse(null);
+        this.salaRepository.delete(sala);
+        return ResponseEntity.ok().body("Registro deletado com sucesso");
+     }
+
 }
--- src/main/java/br/com/uniamerica/rentaclassroom/repository/SalaRepository.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/repository/SalaRepository.java
@@ -2,7 +2,14 @@ package br.com.uniamerica.rentaclassroom.repository;
 
 import br.com.uniamerica.rentaclassroom.entitys.Sala;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SalaRepository extends JpaRepository<Sala, Long> {}
+public interface SalaRepository extends JpaRepository<Sala, Long> {
+
+    public List<Sala> findByAtivo(@Param("ativo") final boolean ativo);
+
+}
