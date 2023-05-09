41a707b510bc98a699576b6e401991a010f99e24
gustavomullerf <gustavoeduardomf@gmail.com>
2023-05-08 22:48:08 -0300

    Criando a função cadastraAgendamento em AgendamentoService

--- src/main/java/br/com/uniamerica/rentaclassroom/services/AgendamentoService.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/services/AgendamentoService.java
@@ -1,6 +1,8 @@
 package br.com.uniamerica.rentaclassroom.services;
 
+import br.com.uniamerica.rentaclassroom.entitys.Agendamento;
 import br.com.uniamerica.rentaclassroom.repositories.AgendamentoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,33 @@ import org.springframework.stereotype.Service;
 public class AgendamentoService {
     @Autowired
     private AgendamentoRepository agendamentoRepository;
+
+    @Transactional
+    public void cadastraAgendamento(Agendamento agendamento){
+        if("".equals(agendamento.getProfessor())){
+            throw new RuntimeException("O campo professor não pode ser nulo");
+        }
+        if("".equals(agendamento.getAmbiente())){
+            throw new RuntimeException("O campo ambiente não pode ser nulo");
+        }
+        if("".equals(agendamento.getData())){
+            throw new RuntimeException("O campo data não pode ser nulo");
+        }
+        if("".equals(agendamento.getPeriodo())){
+            throw new RuntimeException("O campo data não pode ser nulo");
+        }
+        if("".equals(agendamento.getHoraInicio())){
+            throw new RuntimeException("O campo horaInicio não pode ser nulo");
+        }
+        if("".equals(agendamento.getHoraFim())){
+            throw new RuntimeException("O campo horaFim não pode ser nulo");
+        }
+        if("".equals(agendamento.getSelecaoMateriais())){
+            throw new RuntimeException("O campo selecaoMateriais não pode ser nulo");
+        }
+        if("".equals(agendamento.getSituacao())){
+            throw new RuntimeException("O campo situacao não pode ser nulo");
+        }
+        this.agendamentoRepository.save(agendamento);
+    }
 }
