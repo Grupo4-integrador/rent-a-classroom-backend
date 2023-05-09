626790b30cbebf811557226d8beeef601302637c
gustavomullerf <gustavoeduardomf@gmail.com>
2023-05-08 22:20:36 -0300

    Inicializando AgendamentoService

--- src/main/java/br/com/uniamerica/rentaclassroom/services/AgendamentoService.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/services/AgendamentoService.java
@@ -1,4 +1,11 @@
 package br.com.uniamerica.rentaclassroom.services;
 
+import br.com.uniamerica.rentaclassroom.repositories.AgendamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AgendamentoService {
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
 }
