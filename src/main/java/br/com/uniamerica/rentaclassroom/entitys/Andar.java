26cc950096f9f6d6eae1030a962cd5d8a518341e
marcelonv011 <marcelonv011@gmail.com>
2023-05-11 17:40:44 -0300

    modificando enum andar

--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/Andar.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/Andar.java
@@ -1,8 +1,16 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
 public enum Andar {
-  SUBSOLO,
-  PRIMEIRO,
-  SEGUNDO,
-  TERCEIRO,
+  SUBSOLO(1, "Subsolo"),
+  PRIMEIRO(2, "Primeiro"),
+  SEGUNDO(3, "Segundo"),
+  TERCEIRO(4, "Terceiro");
+
+  public final int valorNum;
+  public final String valor;
+
+  private Andar(int valorNum, String valor){
+    this.valorNum = valorNum;
+    this.valor = valor;
+  }
 }
