6f304cbd79ac54d00a83cf89105e3c22d8a0dbe4
marcelonv011 <marcelonv011@gmail.com>
2023-05-11 17:35:55 -0300

    exception que o usuario deve inserir o andar salaservices

--- src/main/java/br/com/uniamerica/rentaclassroom/services/SalaService.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/services/SalaService.java
@@ -42,6 +42,9 @@ public class SalaService {
         if ( sala.getCapacidade() == 0 ){
             throw new RuntimeException(" Deve inserir a capacidade da sala");
         }
+        if ( "".equals(sala.getAndar())){
+            throw new RuntimeException(" Deve inserir o andar");
+        }
         this.salaRepository.save(sala);
     }
 
