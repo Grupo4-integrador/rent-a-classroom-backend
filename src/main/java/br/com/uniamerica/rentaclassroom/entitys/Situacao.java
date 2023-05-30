a71231a6fbfc615b4518c2a8a3b86323f031f417
gustavoemf <gustavoeduardomf@gmail.com>
2023-05-30 11:17:59 -0400

    Configurando Enum Situacao

--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/Situacao.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/Situacao.java
@@ -1,7 +1,15 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
 public enum Situacao {
-  DEFERIDO,
-  INDEFERIDO,
-  EM_AVALIACAO,
+  DEFERIDO (3, "Deferido"),
+  INDEFERIDO (3, "Indeferido"),
+  EM_AVALIACAO (3, "Em avaliação");
+
+  public final int valorNumerico;
+  public final String valorString;
+
+  private Situacao(int valorNumerico, String valorString){
+    this.valorNumerico=valorNumerico;
+    this.valorString=valorString;
+  }
 }
