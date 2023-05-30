4219dc815af7ec03566cefb4ca3d3207d69bfa75
gustavoemf <gustavoeduardomf@gmail.com>
2023-05-30 11:17:42 -0400

    Configurando Enum Andar

--- src/main/java/br/com/uniamerica/rentaclassroom/entitys/Periodo.java
+++ src/main/java/br/com/uniamerica/rentaclassroom/entitys/Periodo.java
@@ -1,7 +1,15 @@
 package br.com.uniamerica.rentaclassroom.entitys;
 
 public enum Periodo {
-  MANHA,
-  TARDE,
-  NOITE,
+  MANHA (1, "Manhã"),
+  TARDE (2, "Tarde"),
+  NOITE (3, "Noite");
+
+  public final int valorNumerico;
+  public final String valorString;
+
+  private Periodo(int valorNumerico, String valorString){
+    this.valorNumerico=valorNumerico;
+    this.valorString=valorString;
+  }
 }
